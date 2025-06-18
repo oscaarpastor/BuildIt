d6bda4c863006e4672d7526dd0f3ca945d450b4b
Oscar Pastor <oscaarpastor@MacBook-Pro-de-Oscar.local>
2025-06-18 17:39:27 +0200

    public

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: ['ec2-52-87-205-103.compute-1.amazonaws.com'],
+    allowedHosts: ['ec2-44-204-149-31.compute-1.amazonaws.com'],
   },
 })
